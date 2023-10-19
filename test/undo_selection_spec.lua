82e6f9656302045a506db1995b8ad3008465a803
Aaron Sullivan <aasullivan1618@gmail.com>
2023-10-18 21:33:36 -0700

    Beginning work on getting simulated selection

--- test/undo_selection_spec.lua
+++ test/undo_selection_spec.lua
@@ -1,9 +1,16 @@
 ---@diagnostic disable: undefined-global
 
+-- TODO get global vim type hints
+
 describe("Undo Selection", function()
   local undo_selection = require("../lua/undo_selection")
 
   it("returns a table with the current visual selection", function()
+    -- Simulate a user making a visual selection
+    -- TODO WIP
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, {"This is a test"})
+    vim.api.nvim_feedkeys('ggVG', 'n', false)
+
     local selection = undo_selection.undo_selection()
     assert.same({start_line = 0, end_line = 0, start_column = 0, end_column = 0}, selection)
   end)
