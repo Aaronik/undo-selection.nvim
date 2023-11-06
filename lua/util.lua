2ad5f432ce0698682709c4282ca1b0f855dc6dc0
Aaron Sullivan <aasullivan1618@gmail.com>
2023-11-05 16:44:55 -0800

    Add indenting to print_table

--- lua/util.lua
+++ lua/util.lua
@@ -2,17 +2,16 @@
 local M = {}
 
 -- function that prints a table
-function M.print_table(t)
+function M.print_table(t, indent)
+  indent = indent or 0
   for k, v in pairs(t) do
     if type(v) == "table" then
-      vim.api.nvim_command("echo '" .. k .. ": '")
-      M.print_table(v)
+      vim.api.nvim_command("echo '" .. string.rep(" ", indent) .. k .. ": '")
+      M.print_table(v, indent + 2)
     else
-      vim.api.nvim_command("echo '" .. k .. ": " .. tostring(v) .. "'")
+      vim.api.nvim_command("echo '" .. string.rep(" ", indent) .. k .. ": " .. tostring(v) .. "'")
     end
   end
-  vim.api.nvim_command("echo 'Press any key to continue'")
-  -- vim.api.nvim_command("silent! call getchar()")
 end
 
 return M
