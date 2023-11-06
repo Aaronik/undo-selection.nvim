fa087f30e2ec87b4d3e178fec509b155f0160d25
Aaron Sullivan <aasullivan1618@gmail.com>
2023-11-05 18:06:58 -0800

    Using some other open source code to help here.
    Originally from https://github.com/debugloop/telescope-undo.nvim

--- lua/undo-selection/module.lua
+++ lua/undo-selection/module.lua
@@ -4,22 +4,143 @@ local M = {}
 local util = require('util')
 
 local function _traverse_undotree(opts, entries, level)
-  -- function body goes here
+  local undolist = {}
+  -- create diffs for each entry in our undotree
+  for i = #entries, 1, -1 do
+    -- grab the buffer as it is after this iteration's undo state
+    vim.cmd("silent undo " .. entries[i].seq)
+    local buffer_after_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false) or {}
+    local buffer_after = table.concat(buffer_after_lines, "\n")
+
+    -- grab the buffer as it is after this undo state's parent
+    vim.cmd("silent undo")
+    local buffer_before_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false) or {}
+    local buffer_before = table.concat(buffer_before_lines, "\n")
+
+    -- create temporary vars and prepare this iteration
+    local diff = ""
+    local ordinal = ""
+    local additions = {}
+    local deletions = {}
+    local on_hunk_callback = function(start_a, count_a, start_b, count_b)
+      -- build diff file header for this hunk, this is important for delta to syntax highlight
+      -- TODO: timestamps are being omitted, but could be included here
+      diff = vim.fn.expand("%")
+      diff = "--- " .. diff .. "\n+++ " .. diff .. "\n"
+      -- build diff location header for this hunk, this is important for delta to show line numbers
+      diff = diff .. "@@ -" .. start_a
+      if count_a ~= 1 then
+        diff = diff .. "," .. count_a
+      end
+      diff = diff .. " +" .. start_b
+      if count_b ~= 1 then
+        diff = diff .. "," .. count_b
+      end
+      diff = diff .. " @@"
+      -- get front context based on options
+      local context_lines = 0
+
+      if opts.diff_context_lines ~= nil then
+        context_lines = opts.diff_context_lines
+      end
+
+      for i = start_a - context_lines, start_a - 1 do
+        if buffer_before_lines[i] ~= nil then
+          diff = diff .. "\n " .. buffer_before_lines[i]
+        end
+      end
+
+      -- get deletions
+      for i = start_a, start_a + count_a - 1 do
+        diff = diff .. "\n-" .. buffer_before_lines[i]
+        table.insert(deletions, buffer_before_lines[i])
+        ordinal = ordinal .. buffer_before_lines[i]
+      end
+
+      -- get additions
+      for i = start_b, start_b + count_b - 1 do
+        diff = diff .. "\n+" .. buffer_after_lines[i]
+        table.insert(additions, buffer_after_lines[i])
+        ordinal = ordinal .. buffer_after_lines[i]
+      end
+
+      -- and finally, get some more context in the back
+      for i = start_a + count_a, start_a + count_a + context_lines - 1 do
+        if buffer_before_lines[i] ~= nil then
+          diff = diff .. "\n " .. buffer_before_lines[i]
+        end
+      end
+
+      -- terminate all this with a newline, so we're ready for the next hunk
+      diff = diff .. "\n"
+    end
+
+    -- do the diff using our internal diff function
+    vim.diff(buffer_before, buffer_after, {
+      result_type = "indices",
+      on_hunk = on_hunk_callback,
+      algorithm = "patience",
+    })
+
+    -- use the data we just created to feed into our finder later
+    table.insert(undolist, {
+      seq = entries[i].seq,                   -- save state number, used in display and to restore
+      alt = level,                            -- current level, i.e. how deep into alt branches are we, used to graph
+      first = i == #entries,                  -- whether this is the first node in this branch, used to graph
+      time = entries[i].time,                 -- save state time, used in display
+      ordinal = ordinal,                      -- a long string of all additions and deletions, used for search
+      diff = diff,                            -- the proper diff, used for preview
+      additions = additions,                  -- all additions, used to yank a result
+      deletions = deletions,                  -- all deletions, used to yank a result
+      bufnr = vim.api.nvim_get_current_buf(), -- for which buffer this telescope was invoked, used to restore
+    })
+
+    -- descend recursively into alternate histories of undo states
+    if entries[i].alt ~= nil then
+      local alt_undolist = _traverse_undotree(opts, entries[i].alt, level + 1)
+      -- pretend these results are our results
+      for _, elem in pairs(alt_undolist) do
+        table.insert(undolist, elem)
+      end
+    end
+  end
+  return undolist
 end
 
-M.traverse_undotree = _traverse_undotree
+local function build_undolist(opts)
+  -- save our current cursor
+  local cursor = vim.api.nvim_win_get_cursor(0)
+
+  -- get all diffs
+  local ut = vim.fn.undotree()
+
+  -- TODO: maybe use this opportunity to limit the number of root nodes we process overall, to ensure good performance
+  local undolist = _traverse_undotree(opts, ut.entries, 0)
+
+  print('undolist:')
+  util.print_table(undolist)
+
+  -- restore everything after all diffs have been created
+  -- BUG: `gi` (last insert location) is being killed by our method, we should save that as well
+  vim.cmd("silent undo " .. ut.seq_cur)
+  vim.api.nvim_win_set_cursor(0, cursor)
+
+  return undolist
+end
 
 -- The main function
 M.undo_selection = function()
-  local selection = M.get_visual_selection()
-  print('selection:')
-  util.print_table(selection)
+  -- local selection = M.get_visual_selection()
+  -- print('selection:')
+  -- util.print_table(selection)
 
-  local changes = M.find_undo_history_for_selection(selection)
-  print('changes')
-  util.print_table(changes)
+  -- local changes = M.find_undo_history_for_selection(selection)
+  -- print('changes')
+  -- util.print_table(changes)
 
-  M.undo_changes(changes)
+  local undolist = build_undolist({})
+  print('undolist')
+  util.print_table(undolist)
 end
 
 -- Just get some data about the current visual selection
@@ -58,8 +179,6 @@ end
 -- * get the visual selection
 -- * find the lines from the undo history
 -- * undo just those lines
-
-return M
 
 -- Here is the documentation for the undotree() function:
 --
@@ -105,3 +224,5 @@ return M
 -- 		  "alt"		Alternate entry.  This is again a List of undo
 -- 				blocks.  Each item may again have an "alt"
 -- 				item.
+
+return M
--- lua/util.lua
+++ lua/util.lua
@@ -7,14 +7,12 @@ function M.print_table(t, indent)
   indent = indent or 0
   for k, v in pairs(t) do
     if type(v) == "table" then
-      vim.api.nvim_command("echo '" .. string.rep(" ", indent) .. k .. ": '")
+      print(string.rep(" ", indent) .. k .. ": ")
       M.print_table(v, indent + 2)
     else
-      vim.api.nvim_command("echo '" .. string.rep(" ", indent) .. k .. ": " .. tostring(v) .. "'")
+      print(string.rep(" ", indent) .. k .. ": " .. tostring(v))
     end
   end
-
-  vim.api.nvim_command("silent! call getchar()")
 end
 
 return M
